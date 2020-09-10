85558a473c969208ec4f4b440fdfbc7f15466823
EricCRodriguez <ecrod12@gmail.com>
2020-09-10 18:00:46 -0500

    fixed two issues with sidebar-still two issues left with sidebar that need to be fixed

--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -4,9 +4,9 @@ import "./SidebarChat.css";
 
 function SidebarChat() {
   return (
-    <div className="SidebarChat">
+    <div className="sidebarChat">
       <Avatar />
-      <div className="SidebarChat__info">
+      <div className="sidebarChat__info">
         <h2>Room Name</h2>
         <p>This is the last messge </p>
       </div>
