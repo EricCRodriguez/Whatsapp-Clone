640a95c0e273dcff3aa11564cf199caa713052a3
EricCRodriguez <ecrod12@gmail.com>
2020-09-10 00:31:41 -0500

    added on sidebar- icons,avatars,search bar-not yet functional

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ import Sidebar from "./Sidebar";
 function App() {
   return (
     <div className="app">
-      <Sidebar />
-      <Chat />
+      <div className="app_body">
+        <Sidebar />
+        <Chat />
+      </div>
     </div>
   );
 }
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,43 @@
 import React from "react";
 import "./Sidebar.css";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import { Avatar, IconButton } from "@material-ui/core";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import { SearchOutlined } from "@material-ui/icons";
+import SidebarChat from "./SidebarChat";
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <h1>I am a sidebar</h1>
+      <div className="sidebar_header">
+        <Avatar src="https://lh3.googleusercontent.com/a-/AOh14Gjw2vOwwkunUf1q5sPoR8-fawO3wtaWcBkrrvksbQ=s88-c-k-c0x00ffffff-no-rj-mo" />
+
+        <div className="sidebar_headerRight">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+
+      <div className="sidebar_search">
+        <div className="sidebar_searchContainer">
+          <SearchOutlined />
+          <input placeholder="Search or start new chat" type="text" />
+        </div>
+      </div>
+
+      <div className="sidebar_chats">
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+      </div>
     </div>
   );
 }
